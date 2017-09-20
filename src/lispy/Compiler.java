573c22975a4dd73239942cccb0edf4a5a07ecbe2
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2017-09-19 19:26:07 -0700

    1st version of the compiler

--- src/lispy/Compiler.java
+++ src/lispy/Compiler.java
@@ -1,5 +1,10 @@
 package lispy;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 public class Compiler {
 
 	public static String jsNamespace = "lispy";
@@ -16,12 +21,36 @@ public class Compiler {
 		builder.append("'<=': function(a,b){ return (a <= b); },");
 		builder.append("'>=': function(a,b){ return (a >= b); },");
 		builder.append("'>' : function(a,b){ return (a > b); },");
+		// TODO add the rest of the built-in functions
 		builder.append("'display': function(s){ if (console){ console.log(s); }; return null; }");
 		builder.append("};");
 		return builder.toString();
 	};
 
-	public static void main(String[] args) {
-		// TODO implement
+	public static void main(String[] args) throws Exception {
+		if (args.length != 1) {
+			System.err.println("Usage: java lispy.Compiler path/to/file.scm\nOutputs 'file.js' in the current directory.");
+			return;
+		}
+
+		String sourceFile = args[0];
+		String namespace = sourceFile.substring(0, sourceFile.lastIndexOf("."));
+		if (namespace.contains("/")) { namespace = namespace.substring(1 + namespace.lastIndexOf("/")); }
+		Compiler.jsNamespace = namespace;
+		String targetFile = String.format("%s.js", namespace);
+		System.out.println(String.format("Output: %s", targetFile));
+
+		BufferedReader reader = new BufferedReader(new FileReader(sourceFile));
+		BufferedWriter writer = new BufferedWriter(new FileWriter(targetFile, false));
+		writer.write(prelude(namespace));
+		String line;
+		Environment env = Environment.getGlobalEnvironment();
+		while ((line = reader.readLine()) != null) {
+			String output = Eval.emit(line, env);
+			writer.write(output + "\n");
+		}
+		reader.close();
+		writer.flush();
+		writer.close();
 	}
 }
