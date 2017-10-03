74ab949e206bdfbe7b9ab0ea76d7afff44defd63
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2017-10-03 08:40:34 -0700

    Repl improvements: blank lines and comments

--- src/lispy/Repl.java
+++ src/lispy/Repl.java
@@ -16,9 +16,18 @@ public class Repl {
 				Scanner sc = new Scanner(System.in);
 				System.out.print("lispy> ");
 				line = sc.nextLine();
-				Object result = Eval.eval(parser.parse(line), env);
-				if (!(result instanceof Environment)) {
-					System.out.println(result);
+
+				// Strip off comments
+				if (line.contains(";")) {
+					line = line.substring(0, line.indexOf(";"));
+				}
+
+				// Attempt parsing only non-whitespace lines
+				if (!(line.matches("\\s*"))) {
+					Object result = Eval.eval(parser.parse(line), env);
+					if (!(result instanceof Environment)) {
+						System.out.println(result);
+					}
 				}
 			} catch (NoSuchElementException nse) {
 				// Thrown when the Scanner can't read a line after a Ctrl-D
